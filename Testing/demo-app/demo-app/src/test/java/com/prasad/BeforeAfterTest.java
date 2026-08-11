c427220278b0874ced7510368d36026f69319145
Prasad <prasadbhakare.5@gmail.com>
2026-08-11 22:07:18 +0530

    BeforeAll and AfterAll annotations

--- Testing/demo-app/demo-app/src/test/java/com/prasad/BeforeAfterTest.java
+++ Testing/demo-app/demo-app/src/test/java/com/prasad/BeforeAfterTest.java
@@ -1,12 +1,19 @@
 package com.prasad;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BeforeAfterTest {
     BeforeAfter obj = new BeforeAfter();
+    
+    @BeforeAll
+    static void bo(){
+        System.out.println("Before all tests");
+    }
     
     @BeforeEach
     void init(){
@@ -16,6 +23,11 @@ public class BeforeAfterTest {
     @AfterEach 
     void outit(){
         System.out.println("After test");
+    }
+
+    @AfterAll
+    static void ao(){
+        System.out.println("After all tests");
     }
 
     @Test
