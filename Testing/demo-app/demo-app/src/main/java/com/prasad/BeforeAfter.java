0a51e9929291dd1f1015d80261e99142c9bba938
Prasad <prasadbhakare.5@gmail.com>
2026-08-11 21:56:55 +0530

    flow control BeforeEach annotation

---
+++ Testing/demo-app/demo-app/src/main/java/com/prasad/BeforeAfter.java
@@ -0,0 +1,14 @@
+package com.prasad;
+
+public class BeforeAfter {
+    public String returnString(){
+        return "Returned";
+    }
+    public int returnInt(){
+        return 0;
+    }
+
+    public Double returnDouble(){
+        return 0.0;
+    }
+}
---
+++ Testing/demo-app/demo-app/src/test/java/com/prasad/BeforeAfterTest.java
@@ -0,0 +1,26 @@
+package com.prasad;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BeforeAfterTest {
+    BeforeAfter obj = new BeforeAfter();
+    
+    @BeforeEach
+    void init(){
+        System.out.println("Before test");
+    }
+
+    @Test
+    void testHello(){
+        System.out.println("Actual Test");
+        assertNotNull(obj.returnString(), () -> "no string object found");
+    }
+
+    @Test
+    void testInt(){
+        System.out.println("Actual Test");
+        assertNotNull(obj.returnInt(), () -> "no int returned");    
+    }
+}
