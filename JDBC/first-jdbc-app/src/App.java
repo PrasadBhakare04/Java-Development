1909c87b8b95db2b17caa600158473cb7967e8fc
Prasad <prasadbhakare.5@gmail.com>
2026-08-19 20:03:13 +0530

    exception handling

--- JDBC/first-jdbc-app/src/App.java
+++ JDBC/first-jdbc-app/src/App.java
@@ -2,31 +2,54 @@
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-
-        //Load and Register the Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        //Establish the Connection
-        String url = System.getenv("URL");
-        String user = System.getenv("USER");
-        String password = System.getenv("PASSWORD");
-        Connection connection = DriverManager.getConnection(url, user, password);
-
-        //Create the Statement
-        Statement statement = connection.createStatement();
+    public static void main(String[] args) {
+        Connection connection=null;
+        Statement statement = null; 
+        try {
+            
+            //Load and Register the Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
     
-        //Execute the Query
-        String sql = "insert into studentInfo values (1, \"Prasad\", 39, \"Pune\")";
-        int rowsAffected = statement.executeUpdate(sql);
+            //Establish the Connection
+            String url = System.getenv("URL");
+            String user = System.getenv("USER");
+            String password = System.getenv("PASSWORD");
+            connection = DriverManager.getConnection(url, user, password);
+    
+            //Create the Statement
+            statement = connection.createStatement();
         
-        //Process the result
-        if(rowsAffected == 0)
-            System.out.println("no data inserted");
-        else
-            System.out.println("Data inserted successfully");
-        //Close the resources
-        connection.close();
+            //Execute the Query
+            String sql = "insert into studentInfo values (1, \"Prasad\", 39, \"Pune\")";
+            int rowsAffected = statement.executeUpdate(sql);
+            
+            //Process the result
+            if(rowsAffected == 0)
+                System.out.println("no data inserted");
+            else
+                System.out.println("Data inserted successfully");
+            
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            //Close the resources
+            try{
+                statement.close();
+                connection.close();
+                
+            }
+            catch(SQLException e){
+
+            }
+        }
     }
 
 }
