7cc89265c7f49464f5f45293be19ee042438e299
Prasad <prasadbhakare.5@gmail.com>
2026-09-24 13:41:13 +0530

    CRUD operations using Hibernate

--- Hibernate/Hibernate_Demo/src/main/java/com/prasad/Main.java
+++ Hibernate/Hibernate_Demo/src/main/java/com/prasad/Main.java
@@ -9,26 +9,72 @@ import org.hibernate.cfg.Configuration;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static SessionFactory factory = new Configuration().addAnnotatedClass(com.prasad.User.class).configure().buildSessionFactory();
+    static Session session = factory.openSession();
+
     static void main() {
         User u1 = new User();
-        u1.setUid(102);
-        u1.setUname("Doe");
-        u1.setTech("Python");
+        u1.setUid(104);
+        u1.setUname("Zoro");
+        u1.setTech("Dev Ops");
 
-        //hey hibernate save the user u1
+        saveUser(u1);
+//        getUser(101);
+//        updateUser(u1);
+//        deleteUser(u1);
+    }
 
-        Configuration config = new Configuration();
-        config.addAnnotatedClass(com.prasad.User.class);
-        config.configure();
+    //hey hibernate save the user u1
+    //Save the data
+    //persist directly runs insert into query
+    public static void saveUser(User u){
+        //We need transaction when we are making changes in the table
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
 
-        SessionFactory factory = config.buildSessionFactory();
-
-        Session session = factory.openSession();
-
-        session.persist(u1);
-
-        Transaction transaction = session.beginTransaction();
+        session.persist(u);
         transaction.commit();
 
+        session.close();
+        factory.close();
+    }
+
+    //Retrieving the data from database
+    public static void getUser(int id){
+        //For fetching the data we don't need transaction
+
+        User u = session.find(User.class,  id);
+        session.close();
+        factory.close();
+        System.out.print(u.getUname());
+    }
+
+    //Update the user in database
+    //this will update if present and insert if the data is not there
+    //merge runs select first to check what is the condition
+    public static void updateUser(User u){
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+
+        session.merge(u);
+
+        transaction.commit();
+        session.close();
+        factory.close();
+    }
+
+    //Delete the user
+    //remove runs select first and then delete
+    public static void deleteUser(User u){
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+
+        session.remove(u);
+
+        transaction.commit();
+
+        session.close();
+        factory.close();
     }
 }
